c43946aa8284a981d62939550f2b9f82e902b422
joscarranzs <jcarranzsosa@gmail.com>
2024-11-01 01:45:36 -0500

    Comments added

--- matching-algorithm/src/main/java/com/unidea/App.java
+++ matching-algorithm/src/main/java/com/unidea/App.java
@@ -3,5 +3,10 @@ package com.unidea;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+
+    /*
+     * Este proyecto se implementara, para crear un algoritmo de coincidencia, enfocado para utilizar
+     * en aplicaciones de citas, con el fin de encontrar personas con el mayor número de coincidencias.
+     */
   }
 }
